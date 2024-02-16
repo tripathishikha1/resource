1272d32c6c04e0ad465a20fb500f66288ab6615a
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-16 21:53:48 +0530

    Update GitCheckoutPrivate.groovy

--- src/org/avengers/common/GitCheckoutPrivate.groovy
+++ src/org/avengers/common/GitCheckoutPrivate.groovy
@@ -1,1 +1,8 @@
-
+package org.avengers.common
+def call(String) {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/OT-MICROSERVICES/employee-api.git/'
+            }
+        }  
+}
