2c6cad7973fc9fb369a0f9a63c946e8ed179a3ba
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-16 22:05:29 +0530

    Update PublishDependencyReport.groovy

--- src/org/avengers/template/PublishDependencyReport.groovy
+++ src/org/avengers/template/PublishDependencyReport.groovy
@@ -1,1 +1,10 @@
+package org.avengers.template
 
+import org.avengers.common.*
+import org.avengers.golang.*
+  
+def call(String){
+  PublishDependencyCheckReport = new PublishDependencyCheckReport()
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  
+}
